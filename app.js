3533ffaada4cd076d78c02c5791391923636a362
Daniel Rodriguez <drodriguez661@gmail.com>
2021-02-24 11:20:08 -0600

    moved files to root

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.querySelector('.banner-btn').addEventListener('click', () =>{
 document.querySelector('.x-btn').addEventListener('click', () => {
   document.querySelector('.banner').style.display = 'flex';
   document.querySelector('.form-container').style.cssText = 'opacity: 0; visibility: hidden';
-  document.querySelector('.container').style.cssText = 'background: linear-graident(rgba(0, 0, 0, .8), rgba(0,0,0, .7)), url(image/bg1.jpg) center no-repeat; background-size: cover';
+  document.querySelector('.container').style.cssText = 'background: linear-graident(rgba(0, 0, 0, .8), rgba(0,0,0, .7)), url(public/image/bg1.jpg) center no-repeat; background-size: cover';
 })
 
 //start of slideshow
